72e2a294c0a81bad502ffb926f67f7f434b4717f
em2046 <em2046@qq.com>
2018-09-30 12:07:48 +0800

    framework

---
+++ src/components/button/__tests__/index.test.jsx
@@ -0,0 +1,74 @@
+import { shallowMount, mount } from "@vue/test-utils";
+import NebulaButton from "../NebulaButton.vue";
+import NebulaIcon from "../../icon/NebulaIcon.vue";
+
+describe("Button", () => {
+  it("renders correctly", () => {
+    const buttonWrapper = shallowMount({
+      render() {
+        return <NebulaButton>Follow</NebulaButton>;
+      }
+    });
+    expect(buttonWrapper.html()).toMatchSnapshot();
+  });
+
+  it("renders Chinese characters correctly", () => {
+    const buttonWrapper1 = mount(NebulaButton, {
+      render() {
+        return <NebulaButton>按钮</NebulaButton>;
+      }
+    });
+    expect(buttonWrapper1.html()).toMatchSnapshot();
+
+    const buttonWrapper2 = mount({
+      render() {
+        return <NebulaButton icon={"search"}>按钮</NebulaButton>;
+      }
+    });
+    expect(buttonWrapper2.html()).toMatchSnapshot();
+
+    const buttonWrapper3 = mount({
+      render() {
+        return (
+          <NebulaButton>
+            <NebulaIcon type={"search"} />
+            按钮
+          </NebulaButton>
+        );
+      }
+    });
+    expect(buttonWrapper3.html()).toMatchSnapshot();
+
+    const buttonWrapper4 = mount({
+      render() {
+        return (
+          <NebulaButton loading>
+            <NebulaIcon type={"search"} />
+            按钮
+          </NebulaButton>
+        );
+      }
+    });
+    expect(buttonWrapper4.html()).toMatchSnapshot();
+
+    const buttonWrapper5 = mount({
+      render() {
+        return <NebulaButton loading>按钮</NebulaButton>;
+      }
+    });
+    expect(buttonWrapper5.html()).toMatchSnapshot();
+  });
+
+  it("should support link button", () => {
+    const wrapper = mount({
+      render() {
+        return (
+          <NebulaButton href={"http://nebula.em2046.com/"} target={"_blank"}>
+            Link button
+          </NebulaButton>
+        );
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
--- tests/unit/example.spec.jsx
+++ tests/unit/example.spec.jsx
@@ -4,8 +4,10 @@ import HelloWorld from "@/components/HelloWorld.vue";
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const wrapper = shallowMount({
+      render() {
+        return <HelloWorld msg={msg} />;
+      }
     });
     expect(wrapper.text()).toMatch(msg);
   });
