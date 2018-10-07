9e5a23228b3804976c23b4d612ff3292d1239b00
em2046 <em2046@qq.com>
2018-10-07 12:50:20 +0800

    vue route

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,12 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <NebulaButton 
-      :label="buttonLabel" 
-      @click="handlerButtonClick">Click me</NebulaButton>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class App extends Vue {
-  public buttonLabel = "按钮";
-  handlerButtonClick() {
-    if (this.buttonLabel === "按钮") {
-      this.buttonLabel = "大按钮";
-    } else {
-      this.buttonLabel = "按钮";
-    }
-  }
-}
-</script>
 
 <style lang="less">
 #app {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./components/nebula-ui.less";
+
 // @ts-ignore
 import { NebulaButton, NebulaIcon, NebulaButtonGroup } from "./index.ts";
+
+import router from './router'
 
 Vue.component("NebulaButton", NebulaButton);
 Vue.component("NebulaIcon", NebulaIcon);
@@ -10,5 +13,6 @@ Vue.component("NebulaButtonGroup", NebulaButtonGroup);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
