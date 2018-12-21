30f2560288fe3a1a382f74864ec8b2396a0705b5
em2046 <em2046@qq.com>
2018-12-21 15:37:03 +0800

    button cleanup code

--- src/components/button/NebulaButton.tsx
+++ src/components/button/NebulaButton.tsx
@@ -1,13 +1,10 @@
-import { Prop, Component, Vue, Watch } from "vue-property-decorator";
+import {Component, Prop, Vue, Watch} from "vue-property-decorator";
 import Wave from "../_util/wave";
 import Icon from "../icon";
+import {mixins} from "vue-class-component";
 
 const rxTwoCNChar = /^[\u4e00-\u9fa5]{2}$/;
 const isTwoCNChar = rxTwoCNChar.test.bind(rxTwoCNChar);
-import buttonTypes from "./buttonTypes";
-import { mixins } from "vue-class-component";
-
-const props = buttonTypes();
 
 export type ButtonType = "default" | "primary" | "ghost" | "dashed" | "danger";
 export type ButtonShape = "circle" | "circle-outline";
@@ -18,39 +15,30 @@ type loadingType = boolean | { delay?: number };
 
 @Component({})
 export class BaseButtonProps extends Vue {
+  @Prop({ default: "nebula-btn", type: String })
+  prefixCls?: string;
+  @Prop({ type: String })
+  type?: ButtonType;
+  @Prop({ default: "button", type: String })
+  htmlType?: ButtonHTMLType;
+  @Prop({ type: String })
+  icon?: string;
+  @Prop({ type: String })
+  shape?: ButtonShape;
+  @Prop({ type: String })
+  size?: ButtonSize;
+  @Prop({ type: [Boolean, Object] })
+  loading?: loadingType;
+  @Prop({ type: Boolean })
+  disabled?: boolean;
+  @Prop({ type: Boolean })
+  ghost?: boolean;
+  @Prop({ type: Boolean })
+  block?: boolean;
+
   constructor(props) {
     super(props);
   }
-
-  @Prop({ default: "nebula-btn", type: String })
-  prefixCls?: string;
-
-  @Prop({ type: String })
-  type?: ButtonType;
-
-  @Prop({ default: "button", type: String })
-  htmlType?: ButtonHTMLType;
-
-  @Prop({ type: String })
-  icon?: string;
-
-  @Prop({ type: String })
-  shape?: ButtonShape;
-
-  @Prop({ type: String })
-  size?: ButtonSize;
-
-  @Prop({ type: [Boolean, Object] })
-  loading?: loadingType;
-
-  @Prop({ type: Boolean })
-  disabled?: boolean;
-
-  @Prop({ type: Boolean })
-  ghost?: boolean;
-
-  @Prop({ type: Boolean })
-  block?: boolean;
 }
 
 @Component({
@@ -58,24 +46,7 @@ export class BaseButtonProps extends Vue {
   __NEBULA_BUTTON: true
 })
 export default class NebulaButton extends mixins(BaseButtonProps) {
-  constructor(props) {
-    super(props);
-  }
-
   static __NEBULA_BUTTON: any = true;
-
-  @Watch("loading")
-  onLoadingChanged(val: loadingType) {
-    clearTimeout(this.delayTimeout);
-    if (typeof val !== "boolean" && val && val.delay) {
-      this.delayTimeout = setTimeout(() => {
-        this.sLoading = !!val;
-      }, val.delay);
-    } else {
-      this.sLoading = !!val;
-    }
-  }
-
   public delayTimeout?: any;
   public sizeMap?: any = {
     large: "lg",
@@ -84,27 +55,12 @@ export default class NebulaButton extends mixins(BaseButtonProps) {
   public sLoading?: any = !!this.loading;
   public hasTwoCNChar?: any = false;
 
-  initData() {}
-
-  created() {
-    this.initData();
-  }
-
-  mounted() {
-    this.fixTwoCNChar();
-  }
-
-  updated() {
-    this.fixTwoCNChar();
-  }
-
-  beforeDestroy() {
-    // if (this.timeout) {
-    //   clearTimeout(this.timeout)
-    // }
-    if (this.delayTimeout) {
-      clearTimeout(this.delayTimeout);
-    }
+  constructor(props) {
+    super(props);
+    Object.assign(this, {
+      loading: props.loading,
+      hasTwoCNChar: false
+    });
   }
 
   get classes() {
@@ -130,6 +86,35 @@ export default class NebulaButton extends mixins(BaseButtonProps) {
       [`${prefixCls}-two-chinese-chars`]: hasTwoCNChar,
       [`${prefixCls}-block`]: block
     };
+  }
+
+  @Watch("loading")
+  onLoadingChanged(val: loadingType) {
+    clearTimeout(this.delayTimeout);
+    if (typeof val !== "boolean" && val && val.delay) {
+      this.delayTimeout = setTimeout(() => {
+        this.sLoading = !!val;
+      }, val.delay);
+    } else {
+      this.sLoading = !!val;
+    }
+  }
+
+  mounted() {
+    this.fixTwoCNChar();
+  }
+
+  updated() {
+    this.fixTwoCNChar();
+  }
+
+  beforeDestroy() {
+    // if (this.timeout) {
+    //   clearTimeout(this.timeout)
+    // }
+    if (this.delayTimeout) {
+      clearTimeout(this.delayTimeout);
+    }
   }
 
   fixTwoCNChar() {
