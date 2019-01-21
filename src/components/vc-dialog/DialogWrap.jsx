c7ddf80fa8dc02f60f63c1de5f4db6026abc8520
em2046 <em2046@qq.com>
2019-01-21 17:09:11 +0800

    modal

--- src/components/vc-dialog/DialogWrap.jsx
+++ src/components/vc-dialog/DialogWrap.jsx
@@ -1,70 +1,67 @@
-
-import Dialog from './Dialog'
-import ContainerRender from '../_util/ContainerRender'
-import getDialogPropTypes from './IDialogPropTypes'
-import { getStyle, getClass } from '../_util/props-util'
-const IDialogPropTypes = getDialogPropTypes()
+import Dialog from "./Dialog";
+import ContainerRender from "../_util/ContainerRender";
+import getDialogPropTypes from "./IDialogPropTypes";
+import { getStyle, getClass } from "../_util/props-util";
+const IDialogPropTypes = getDialogPropTypes();
 const DialogWrap = {
   props: {
     ...IDialogPropTypes,
-    visible: IDialogPropTypes.visible.def(false),
+    visible: IDialogPropTypes.visible.def(false)
   },
-  data () {
-    this.renderComponent = () => {}
-    this.removeContainer = () => {}
-    return {}
+  data() {
+    this.renderComponent = () => {};
+    this.removeContainer = () => {};
+    return {};
   },
 
-  beforeDestroy () {
+  beforeDestroy() {
     if (this.visible) {
       this.renderComponent({
         afterClose: this.removeContainer,
         visible: false,
         on: {
-          close () {},
-        },
-      })
+          close() {}
+        }
+      });
     } else {
-      this.removeContainer()
+      this.removeContainer();
     }
   },
   methods: {
-    getComponent (extra = {}) {
-      const { $attrs, $listeners, $props, $slots } = this
-      const { on, ...otherProps } = extra
+    getComponent(extra = {}) {
+      const { $attrs, $listeners, $props, $slots } = this;
+      const { on, ...otherProps } = extra;
       const dialogProps = {
         props: {
           ...$props,
           dialogClass: getClass(this),
           dialogStyle: getStyle(this),
-          ...otherProps,
+          ...otherProps
         },
         attrs: $attrs,
-        ref: '_component',
-        key: 'dialog',
+        ref: "_component",
+        key: "dialog",
         on: {
           ...$listeners,
-          ...on,
-        },
-      }
-      return (
-        <Dialog {...dialogProps}>{$slots.default}</Dialog>
-      )
+          ...on
+        }
+      };
+      return <Dialog {...dialogProps}>{$slots.default}</Dialog>;
     },
 
-    getContainer2 () {
-      const container = document.createElement('div')
+    getContainer2() {
+      const container = document.createElement("div");
       if (this.getContainer) {
-        this.getContainer().appendChild(container)
+        this.getContainer().appendChild(container);
       } else {
-        document.body.appendChild(container)
+        document.body.appendChild(container);
       }
-      return container
-    },
+      return container;
+    }
   },
 
-  render () {
-    const { visible } = this
+  render() {
+    const { visible } = this;
     return (
       <ContainerRender
         parent={this}
@@ -73,14 +70,13 @@ const DialogWrap = {
         getComponent={this.getComponent}
         getContainer={this.getContainer2}
         children={({ renderComponent, removeContainer }) => {
-          this.renderComponent = renderComponent
-          this.removeContainer = removeContainer
-          return null
+          this.renderComponent = renderComponent;
+          this.removeContainer = removeContainer;
+          return null;
         }}
       />
-    )
-  },
-}
+    );
+  }
+};
 
-export default DialogWrap
-
+export default DialogWrap;
